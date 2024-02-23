d65cbb300dca5cbc8e9f7bb6d43afd0323cd2396
Shivam <110303492+st412613@users.noreply.github.com>
2024-02-23 07:39:47 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -13,18 +13,25 @@ app.use(cors());
 app.use(bodyParser.json());
 app.get('/', (req, res) => {
     res.send("hello world from backend");
-    
+
 });
 // POST endpoint for translation
 app.post('/translate', async (req, res) => {
     try {
         // Check if request body contains the text to translate
+        console.log('Request received:', req.body);
+
+        // Check if request body contains the text to translate
         if (!req.body || !req.body.text) {
             return res.status(400).json({ error: 'Request body must contain a "text" field.' });
         }
 
+        console.log('Translating:', req.body.text);
+
         // Translate the text
         const translatedText = await translate(req.body.text, { from: 'en', to: 'fr' });
+
+        console.log('Translation:', translatedText);
 
         // Send back the translated text
         res.json({ translation: translatedText });
